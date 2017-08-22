5bea5eb7defe34b6ffc44da8e04eae245284a8c6
louietsai <louietsai@gmail.com>
2017-08-22 08:02:51 -0700

    [feature] undistort and transform according to camera cal results

--- camera_calibration.py
+++ camera_calibration.py
@@ -40,10 +40,10 @@ for idx, fname in enumerate(images):
         cv2.drawChessboardCorners(img, (9,6), corners, ret)
         #write_name = 'out/corners_found'+str(idx)+'.jpg'
         #cv2.imwrite(write_name, img)
-        cv2.imshow('img', img)
-        cv2.waitKey(500)
+        #cv2.imshow('img', img)
+        #cv2.waitKey(500)
 
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
 
 
 # ### If the above cell ran sucessfully, you should now have `objpoints` and `imgpoints` needed for camera calibration.  Run the cell below to calibrate, calculate distortion coefficients, and test undistortion on an image!
@@ -61,8 +61,8 @@ img_size = (img.shape[1], img.shape[0])
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
 
 
-dst = cv2.undistort(img, mtx, dist, None, mtx)
-cv2.imwrite('output_images/calibration_undist.jpg',dst)
+#dst = cv2.undistort(img, mtx, dist, None, mtx)
+#cv2.imwrite('output_images/calibration_undist.jpg',dst)
 
 # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
 dist_pickle = {}
@@ -71,15 +71,92 @@ dist_pickle["dist"] = dist
 pickle.dump( dist_pickle, open( "camera_cal/wide_dist_pickle.p", "wb" ) )
 #dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
 # Visualize undistortion
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
-ax1.imshow(img)
-ax1.set_title('Original Image', fontsize=30)
-ax2.imshow(dst)
-ax2.set_title('Undistorted Image', fontsize=30)
+#f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+#ax1.imshow(img)
+#ax1.set_title('Original Image', fontsize=30)
+#ax2.imshow(dst)
+#ax2.set_title('Undistorted Image', fontsize=30)
 
 
 # In[ ]:
 
+#import pickle
+#import cv2
+#import numpy as np
+#import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
+# Read in the saved camera matrix and distortion coefficients
+# These are the arrays you calculated using cv2.calibrateCamera()
+dist_pickle = pickle.load( open( "camera_cal/wide_dist_pickle.p", "rb" ) )
+mtx = dist_pickle["mtx"]
+dist = dist_pickle["dist"]
 
+# Read in an image
+#img = cv2.imread('test_image2.png')
+#img = cv2.imread('camera_cal/calibration2.jpg')
+img = cv2.imread('camera_cal/calibration13.jpg')
+nx = 9 # the number of inside corners in x
+ny = 6 # the number of inside corners in y
 
+# MODIFY THIS FUNCTION TO GENERATE OUTPUT 
+# THAT LOOKS LIKE THE IMAGE ABOVE
+def corners_unwarp(img, nx, ny, mtx, dist):
+    # Pass in your image into this function
+    # Write code to do the following steps
+    # 1) Undistort using mtx and dist
+    ret, corners_orig = cv2.findChessboardCorners(img, (9,6),None)
+    #print('--------')
+    #print(corners_orig)
+    dst = cv2.undistort(img, mtx, dist, None, None)
+    # 2) Convert to grayscale
+    gray = cv2.cvtColor(dst,cv2.COLOR_BGR2GRAY)
+    # 3) Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+    #print('========')
+    #print(corners)
+    # 4) If corners found: 
+    M = None
+    warped = None
+    if ret == True:
+            # a) draw corners
+            cv2.drawChessboardCorners(gray, (9, 6), corners, ret)
+            # b) define 4 source points src = np.float32([[,],[,],[,],[,]])
+                 #Note: you could pick any four of the detected corners 
+                 # as long as those four corners define a rectangle
+                 #One especially smart way to do this would be to use four well-chosen
+                 # corners that were automatically detected during the undistortion steps
+                 #We recommend using the automatic detection of corners in your code
+            offset = 100 # offset for dst points
+            # Grab the image shape
+            img_size = (gray.shape[1], gray.shape[0])
+
+            # For source points I'm grabbing the outer four detected corners
+            src = np.float32([corners[0], corners[nx-1], corners[-1], corners[-nx]])
+            # For destination points, I'm arbitrarily choosing some points to be
+            # a nice fit for displaying our warped result 
+            # again, not exact, but close enough for our purposes
+            dst = np.float32([[offset, offset], [img_size[0]-offset, offset], 
+                                     [img_size[0]-offset, img_size[1]-offset], 
+                                     [offset, img_size[1]-offset]])
+            # d) use cv2.getPerspectiveTransform() to get M, the transform matrix
+            #cv2.getPerspectiveTransform(src,dst,M)
+            M = cv2.getPerspectiveTransform(src, dst)
+	        # e) use cv2.warpPerspective() to warp your image to a top-down view
+            #cv2.warpPerspective(gray, final, M)
+            #print(nx,ny)
+            warped = cv2.warpPerspective(gray, M,(1280,960))
+    #delete the next two lines
+    #M = None
+    #img = warped
+    #warped = np.copy(img) 
+    return warped, M
+
+top_down, perspective_M = corners_unwarp(img, nx, ny, mtx, dist)
+print("after corner_unwarp")
+cv2.imshow('img', img)
+cv2.waitKey(500)
+cv2.imshow('top_down', top_down)
+cv2.waitKey(1000)
+write_name = 'output_images/calibration13.jpg'
+cv2.imwrite(write_name, top_down)
